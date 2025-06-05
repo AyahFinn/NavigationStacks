889101b894b4386c913237e7de3a3faa7964a17c
Ayah <ayah29393@gmail.com>
2025-06-05 15:44:59 -0500

    Initial Commit

---
+++ NavigationStacks/NavigationStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStacksApp.swift
+//  NavigationStacks
+//
+//  Created by Scholar on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationStacksTests/NavigationStacksTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStacksTests.swift
+//  NavigationStacksTests
+//
+//  Created by Scholar on 6/5/25.
+//
+
+import Testing
+@testable import NavigationStacks
+
+struct NavigationStacksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
